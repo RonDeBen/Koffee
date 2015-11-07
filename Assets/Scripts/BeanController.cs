f5a33c8a91ecee1ba58296735f1eb05aedf50948
Ron De Benedetti <RonDeBen@gmail.com>
2015-11-07 12:27:26 -0600

    dropping beans now spawns beans

--- Assets/Scripts/BeanController.cs
+++ Assets/Scripts/BeanController.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class BeanController : MonoBehaviour {
 
 	void Start(){
-		Debug.Log(gameObject.GetComponent<BoxCollider2D>().isTrigger);
 	}
 
 
---
+++ Assets/Scripts/MyBean.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyBean : MonoBehaviour {
+
+	void OnBecameInvisible(){
+		SpriteRenderer sprender = gameObject.GetComponent<SpriteRenderer>();
+		sprender.enabled = false;
+		Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+		rb.isKinematic = true;
+		rb.gravityScale = 0f;
+		GameObject player = GameObject.FindWithTag("Player");
+		gameObject.transform.position = player.transform.position;
+		BeanSpawner.SpawnNewBean();
+	}
+}
