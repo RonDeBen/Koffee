1e109d76eb6d6c57bf4eaa43d884491cbc6f28fd
Ron De Benedetti <RonDeBen@gmail.com>
2015-11-07 18:22:26 -0600

    fixes the bean issue

--- Assets/Scripts/MyBean.cs
+++ Assets/Scripts/MyBean.cs
@@ -6,13 +6,15 @@ public class MyBean : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other){
 		if(other.tag == "coffee_pot"){
 			SpriteRenderer sprender = gameObject.GetComponent<SpriteRenderer>();
-			sprender.enabled = false;
-			Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
-			rb.isKinematic = true;
-			rb.gravityScale = 0f;
-			GameObject player = GameObject.FindWithTag("Player");
-			gameObject.transform.position = player.transform.position;
-			BeanSpawner.SpawnNewBean();
+			if(sprender.enabled){
+				sprender.enabled = false;
+				Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+				rb.isKinematic = true;
+				rb.gravityScale = 0f;
+				GameObject player = GameObject.FindWithTag("Player");
+				gameObject.transform.position = player.transform.position;
+				BeanSpawner.SpawnNewBean();
+			}
 		}
 	}
 }
