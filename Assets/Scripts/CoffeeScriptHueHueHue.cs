7efb1d3ac13a43e39711a4edce20b2a7c6056e37
Ron De Benedetti <RonDeBen@gmail.com>
2015-11-08 05:45:06 -0600

    adds playerprefs shit and a level thingy

--- Assets/Scripts/CoffeeScriptHueHueHue.cs
+++ Assets/Scripts/CoffeeScriptHueHueHue.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CoffeeScriptHueHueHue : MonoBehaviour {
 
 	public Sprite[] coffeeProgressSprites;
+	public GameObject panelObj;
+	public Text gameOverTextObj;
+	public Color gameOverColor;
+	private static Color panelColor;
 	private static Sprite[] sprites;
 	private static SpriteRenderer sr;
+	private static Image canvasImage;
+	private static Text gameOverText;
 
 	private static int beanCount = 0;
 
@@ -14,23 +21,40 @@ public class CoffeeScriptHueHueHue : MonoBehaviour {
 	void Start () {
 		sr = gameObject.GetComponent<SpriteRenderer>();
 		sprites = coffeeProgressSprites;
+		panelColor = gameOverColor;
+		canvasImage = panelObj.GetComponent<Image>();
+		gameOverText = gameOverTextObj;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (gameOverText.enabled && Input.GetKeyDown(KeyCode.R)){
+			RestartGame();
+		}
+	}
+
+	public static void RestartGame(){
+		beanCount = 0;
+		Application.LoadLevel("final");
+		Time.timeScale = 1;
 	}
 
 	public static void ReceiveBean(){
 		sr.sprite = sprites[beanCount];
 		if(beanCount == sprites.Length - 1){
-			//you win the game
+			PlayerPrefs.SetInt("Score", Score.GetScore());
+			PlayerPrefs.SetInt("Level", Score.GetLevel() + 1);
+			RestartGame();
 		}
 		beanCount++;
 	}
 
 	public static void ReceiveEnemy(){
 		//you lose the game
+		PlayerPrefs.DeleteKey("Score");
+		canvasImage.color = panelColor;
+		gameOverText.enabled = true;
+		Time.timeScale = 0;
 	}
 
 }
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -4,14 +4,34 @@ using System.Collections;
 
 public class Score : MonoBehaviour {
 
-public Text scoreText;
-private static int score;
+public Text scoreText, levelText;
+private static int score, level;
+
+	void Start(){
+		score = PlayerPrefs.GetInt("Score");
+		level = PlayerPrefs.GetInt("Level");
+
+		if (level == 0){
+			level = 1;
+			PlayerPrefs.SetInt("Level", 1);
+		}
+	}
 
 	void Update(){
 		scoreText.text = "Score: " + score;
+		levelText.text = "Level: " + level;
 	}
+
 
 	public static void AddPoints(int value){
 		score += value;
+	}
+
+	public static int GetScore(){
+		return score;
+	}
+
+	public static int GetLevel(){
+		return level;
 	}
 }
